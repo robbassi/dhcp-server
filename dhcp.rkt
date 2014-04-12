11b261c8a0c0624050ea8b123f59fe0b35e89163
Rob Bassi <rbassi@venasolutions.com>
2014-04-12 19:15:17 -0400

    dhcp now provides functions from packet.rkt

--- dhcp.rkt
+++ dhcp.rkt
@@ -3,12 +3,15 @@
 (require racket/udp
          "packet.rkt")
 
-(define (dhcp-discover? dhcp-packet) 
-  (equal? (hash-ref! (dhcp-packet-options dhcp-packet)))
-(define (dhcp-request? dhcp-packet) )
-(define (dhcp-decline? dhcp-packet) )
-(define (dhcp-release? dhcp-packet) )
-(define (dhcp-inform? dhcp-packet) )
-  
-(define (verify-option dhcp-packet code value) 
-  )
+(provide dhcp-packet-type
+         bytes->dhcp-packet
+         dhcp-packet->bytes
+         (struct-out dhcp-packet))
+
+(define (dhcp-packet-type dhcp-packet)
+  (match (hash-ref (dhcp-packet-options dhcp-packet) 53)
+    [1 'discover]
+    [3 'request]
+    [4 'decline]
+    [7 'release]
+    [8 'inform]))
